4691f7ba09936fcad1b320380febd4cebd26b7f9
Derek Chen <derex1987@gmail.com>
2025-06-23 04:28:12 -0500

    feat: nerf fly speed to 0.3

--- src/main/kotlin/io/github/derec4/fasterHappyGhast/listeners/HappyGhastSpawnListener.kt
+++ src/main/kotlin/io/github/derec4/fasterHappyGhast/listeners/HappyGhastSpawnListener.kt
@@ -15,6 +15,6 @@ class HappyGhastSpawnListener : Listener {
             return
         }
 
-        happyGhast.getAttribute(Attribute.FLYING_SPEED)?.baseValue = 0.5
+        happyGhast.getAttribute(Attribute.FLYING_SPEED)?.baseValue = 0.3
     }
 }
