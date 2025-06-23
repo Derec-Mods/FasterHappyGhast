198f78e66643af224a5620d573ce38f667d39483
Derek Chen <derex1987@gmail.com>
2025-06-23 04:28:00 -0500

    feat: set flying speed to 0.5 by default

--- src/main/kotlin/io/github/derec4/fasterHappyGhast/listeners/HappyGhastSpawnListener.kt
+++ src/main/kotlin/io/github/derec4/fasterHappyGhast/listeners/HappyGhastSpawnListener.kt
@@ -1,6 +1,7 @@
 package io.github.derec4.fasterHappyGhast.listeners
 
-import io.papermc.paper.registry.keys.tags.EntityTypeTagKeys
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.HappyGhast
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.CreatureSpawnEvent
@@ -8,8 +9,12 @@ import org.bukkit.event.entity.CreatureSpawnEvent
 class HappyGhastSpawnListener : Listener {
     @EventHandler
     fun onHappyGhastSpawn(event: CreatureSpawnEvent) {
-        val happyGhast = event.entity;
+        val happyGhast = event.entity
 
-        if(happyGhast)
+        if (happyGhast !is HappyGhast) {
+            return
+        }
+
+        happyGhast.getAttribute(Attribute.FLYING_SPEED)?.baseValue = 0.5
     }
 }
