e86f4b163db5ac96c9ab83a9c83fb7769b70f31d
Derek Chen <derex1987@gmail.com>
2026-01-19 18:31:41 -0600

    chore: i did the bugfix but accidentally messed up gitignore, will fix at home

---
+++ bin/main/io/github/derec4/fasterHappyGhast/handlers/ConfigHandler.kt
@@ -0,0 +1,63 @@
+package io.github.derec4.fasterHappyGhast.handlers
+
+import org.bukkit.configuration.file.FileConfiguration
+import org.bukkit.plugin.java.JavaPlugin
+
+class ConfigHandler {
+    var baseSpeed: Double = 0.17
+    var defaultSpeed: Double = 0.05
+    var affectAll: Boolean = false
+
+    fun generateDefaultConfig(config: FileConfiguration) {
+        config.options().setHeader(
+            listOf(
+                "--------------------------------------------------",
+                "Happy Ghast vanilla friendly plugin by DerexXD",
+                "Report all ur troubles at the link below!",
+                "https://github.com/Derec-Mods/FasterHappyGhast/issues",
+                "--------------------------------------------------"
+            )
+        )
+        config.setComments(
+            "base-speed", listOf(
+                "By default only riding a happy ghast will boost its speed; dismounting will return it to normal",
+                "Speed used for the Happy Ghast (attribute:flying_speed base in vanilla mob stats)"
+            )
+        )
+        config.addDefault("base-speed", baseSpeed)
+        config.setComments(
+            "default-speed", listOf(
+                "Speed used for the Happy Ghast when not mounted (attribute:flying_speed base in vanilla mob stats)"
+            )
+        )
+        config.addDefault("default-speed", defaultSpeed)
+//        config.setComments(
+//            "always-affect-tamed-ghasts", listOf(
+//                "If the increased speed should happen always (not only when a player is riding the Happy Ghast)"
+//            )
+//        )
+//        config.addDefault("always-affect-tamed-ghasts", affectHarnessOnly)
+        config.setComments(
+            "affect-untamed-happy-ghasts", listOf(
+                "If the plugin should make untamed Happy Ghasts faster too"
+            )
+        )
+        config.addDefault("affect-untamed-happy-ghasts", affectAll)
+        config.options().copyDefaults(true)
+    }
+
+    fun load(plugin: JavaPlugin) {
+        val config = plugin.config
+        baseSpeed = config.getDouble("base-speed", 0.17)
+        defaultSpeed = config.getDouble("default-speed", 0.05)
+        affectAll = config.getBoolean("affect-untamed-happy-ghasts", false)
+    }
+
+    fun save(plugin: JavaPlugin) {
+        val config = plugin.config
+        config.set("base-speed", baseSpeed)
+        config.set("default-speed", defaultSpeed)
+        config.set("affect-untamed-happy-ghasts", affectAll)
+        plugin.saveConfig()
+    }
+}
---
+++ bin/main/io/github/derec4/fasterHappyGhast/listeners/HappyGhastTameListener.kt
@@ -0,0 +1,22 @@
+package io.github.derec4.fasterHappyGhast.listeners
+
+import io.github.derec4.fasterHappyGhast.FasterHappyGhast
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.HappyGhast
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityTameEvent
+
+class HappyGhastTameListener: Listener {
+    @EventHandler
+    fun onHappyGhastTame (event: EntityTameEvent) {
+//        println("[DEBUG] HappyGhastTameListener triggered")
+        val happyGhast = event.entity;
+
+        if (happyGhast !is HappyGhast) {
+            return;
+        }
+
+        happyGhast.getAttribute(Attribute.FLYING_SPEED)?.baseValue = FasterHappyGhast.baseSpeed;
+    }
+}
